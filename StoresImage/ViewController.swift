6bd4e68d19dbc3e7604d87bf6c71cc9c7b190d12
Nguyen Minh Thanh <thanh91pro@gmail.com>
2019-11-15 15:53:14 +0700

    Initial Commit

---
+++ StoresImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoresImage
+//
+//  Created by administrator on 11/15/19.
+//  Copyright © 2019 Vision. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
